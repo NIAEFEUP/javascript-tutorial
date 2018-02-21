68b03b7ef016015069c54b35cc83d03e56a3c3dc
Marcelo Ferreira <pount@sapo.pt>
2018-02-21 23:22:43 +0000

    added and updated files with the flappy code

--- sketch.js
+++ sketch.js
@@ -13,12 +13,33 @@ function setup() {
 function draw() {
   background(0);
 
+  for (var i = pipes.length-1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)) {
+      console.log("HIT");
+    }
+
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+
+  }
+
+  bird.update();
+  bird.show();
+
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+  }
+
 }
 
 
 // Aqui e onde devem identificar a tecla que querem usar e a respetiva ação
 function keyPressed() {
   if (key == ' ') {
-   
+    bird.up();
   }
 }
